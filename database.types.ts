9975437647d35947b872867752a8fdfc75526ba7
Le Viet Hoang <hashdotlee@gmail.com>
2024-12-02 17:56:41 +0700

    fix linting

--- database.types.ts
+++ database.types.ts
@@ -4,112 +4,112 @@ export type Json =
   | boolean
   | null
   | { [key: string]: Json | undefined }
-  | Json[]
+  | Json[];
 
 export type Database = {
   public: {
     Tables: {
       bot_documents: {
         Row: {
-          bot_id: number
-          created_at: string
-          document_id: number
-        }
+          bot_id: number;
+          created_at: string;
+          document_id: number;
+        };
         Insert: {
-          bot_id: number
-          created_at?: string
-          document_id: number
-        }
+          bot_id: number;
+          created_at?: string;
+          document_id: number;
+        };
         Update: {
-          bot_id?: number
-          created_at?: string
-          document_id?: number
-        }
+          bot_id?: number;
+          created_at?: string;
+          document_id?: number;
+        };
         Relationships: [
           {
-            foreignKeyName: "bot_documents_bot_id_fkey"
-            columns: ["bot_id"]
-            isOneToOne: false
-            referencedRelation: "bots"
-            referencedColumns: ["id"]
+            foreignKeyName: "bot_documents_bot_id_fkey";
+            columns: ["bot_id"];
+            isOneToOne: false;
+            referencedRelation: "bots";
+            referencedColumns: ["id"];
           },
           {
-            foreignKeyName: "bot_documents_document_id_fkey"
-            columns: ["document_id"]
-            isOneToOne: false
-            referencedRelation: "documents"
-            referencedColumns: ["id"]
+            foreignKeyName: "bot_documents_document_id_fkey";
+            columns: ["document_id"];
+            isOneToOne: false;
+            referencedRelation: "documents";
+            referencedColumns: ["id"];
           },
-        ]
-      }
+        ];
+      };
       bots: {
         Row: {
-          api_key: string | null
-          created_at: string
-          id: number
-          name: string | null
-          prompt: string | null
-        }
+          api_key: string | null;
+          created_at: string;
+          id: number;
+          name: string | null;
+          prompt: string | null;
+        };
         Insert: {
-          api_key?: string | null
-          created_at?: string
-          id?: number
-          name?: string | null
-          prompt?: string | null
-        }
+          api_key?: string | null;
+          created_at?: string;
+          id?: number;
+          name?: string | null;
+          prompt?: string | null;
+        };
         Update: {
-          api_key?: string | null
-          created_at?: string
-          id?: number
-          name?: string | null
-          prompt?: string | null
-        }
-        Relationships: []
-      }
+          api_key?: string | null;
+          created_at?: string;
+          id?: number;
+          name?: string | null;
+          prompt?: string | null;
+        };
+        Relationships: [];
+      };
       documents: {
         Row: {
-          content: string | null
-          created_at: string
-          id: number
-          selector: string | null
-          type: number | null
-          url: string | null
-        }
+          content: string | null;
+          created_at: string;
+          id: number;
+          selector: string | null;
+          type: number | null;
+          url: string | null;
+        };
         Insert: {
-          content?: string | null
-          created_at?: string
-          id?: number
-          selector?: string | null
-          type?: number | null
-          url?: string | null
-        }
+          content?: string | null;
+          created_at?: string;
+          id?: number;
+          selector?: string | null;
+          type?: number | null;
+          url?: string | null;
+        };
         Update: {
-          content?: string | null
-          created_at?: string
-          id?: number
-          selector?: string | null
-          type?: number | null
-          url?: string | null
-        }
-        Relationships: []
-      }
-    }
+          content?: string | null;
+          created_at?: string;
+          id?: number;
+          selector?: string | null;
+          type?: number | null;
+          url?: string | null;
+        };
+        Relationships: [];
+      };
+    };
     Views: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     Functions: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     Enums: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     CompositeTypes: {
-      [_ in never]: never
-    }
-  }
-}
+      [_ in never]: never;
+    };
+  };
+};
 
-type PublicSchema = Database[Extract<keyof Database, "public">]
+type PublicSchema = Database[Extract<keyof Database, "public">];
 
 export type Tables<
   PublicTableNameOrOptions extends
@@ -122,7 +122,7 @@ export type Tables<
 > = PublicTableNameOrOptions extends { schema: keyof Database }
   ? (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
       Database[PublicTableNameOrOptions["schema"]]["Views"])[TableName] extends {
-      Row: infer R
+      Row: infer R;
     }
     ? R
     : never
@@ -130,11 +130,11 @@ export type Tables<
         PublicSchema["Views"])
     ? (PublicSchema["Tables"] &
         PublicSchema["Views"])[PublicTableNameOrOptions] extends {
-        Row: infer R
+        Row: infer R;
       }
       ? R
       : never
-    : never
+    : never;
 
 export type TablesInsert<
   PublicTableNameOrOptions extends
@@ -145,17 +145,17 @@ export type TablesInsert<
     : never = never,
 > = PublicTableNameOrOptions extends { schema: keyof Database }
   ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-      Insert: infer I
+      Insert: infer I;
     }
     ? I
     : never
   : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
     ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
-        Insert: infer I
+        Insert: infer I;
       }
       ? I
       : never
-    : never
+    : never;
 
 export type TablesUpdate<
   PublicTableNameOrOptions extends
@@ -166,17 +166,17 @@ export type TablesUpdate<
     : never = never,
 > = PublicTableNameOrOptions extends { schema: keyof Database }
   ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-      Update: infer U
+      Update: infer U;
     }
     ? U
     : never
   : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
     ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
-        Update: infer U
+        Update: infer U;
       }
       ? U
       : never
-    : never
+    : never;
 
 export type Enums<
   PublicEnumNameOrOptions extends
@@ -189,14 +189,14 @@ export type Enums<
   ? Database[PublicEnumNameOrOptions["schema"]]["Enums"][EnumName]
   : PublicEnumNameOrOptions extends keyof PublicSchema["Enums"]
     ? PublicSchema["Enums"][PublicEnumNameOrOptions]
-    : never
+    : never;
 
 export type CompositeTypes<
   PublicCompositeTypeNameOrOptions extends
     | keyof PublicSchema["CompositeTypes"]
     | { schema: keyof Database },
   CompositeTypeName extends PublicCompositeTypeNameOrOptions extends {
-    schema: keyof Database
+    schema: keyof Database;
   }
     ? keyof Database[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"]
     : never = never,
@@ -204,4 +204,4 @@ export type CompositeTypes<
   ? Database[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"][CompositeTypeName]
   : PublicCompositeTypeNameOrOptions extends keyof PublicSchema["CompositeTypes"]
     ? PublicSchema["CompositeTypes"][PublicCompositeTypeNameOrOptions]
-    : never
+    : never;
