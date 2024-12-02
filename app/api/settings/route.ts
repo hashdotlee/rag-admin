dfb3e4b6a131fe638f64295ee0fe9be100636d5b
Le Viet Hoang <hashdotlee@gmail.com>
2024-12-02 17:17:31 +0700

    fix linting

--- app/api/settings/route.ts
+++ app/api/settings/route.ts
@@ -1,24 +1,25 @@
 import { supabase } from "@/lib/supabase";
-import { NextRequest, NextResponse } from "next/server";
 
-export const GET = async ({ request }: { request: NextRequest }) => {
-  const { API_KEY, BOT_NAME } = await request.json();
+export const GET = async (request: Request) => {
+  try {
+    const { API_KEY, BOT_NAME } = await request.json();
 
-  const { data, error } = await supabase
-    .from("bots")
-    .select("*")
-    .eq("name", BOT_NAME)
-    .single();
+    const { data, error } = await supabase
+      .from("bots")
+      .select("*")
+      .eq("name", BOT_NAME)
+      .single();
 
-  if (!error && !data) {
-    return new NextResponse("Bot not found", { status: 404 });
+    if (!error && !data) {
+      return new Response("Bot not found", { status: 404 });
+    }
+
+    if (data?.api_key !== API_KEY) {
+      return new Response("Unauthorized", { status: 401 });
+    }
+
+    return Response.json(data);
+  } catch (error) {
+    return new Response("Internal Server Error", { status: 500 });
   }
-
-  if (data?.api_key !== API_KEY) {
-    return new NextResponse("Unauthorized", { status: 401 });
-  }
-
-  return new NextResponse(JSON.stringify(data), {
-    headers: { "content-type": "application/json" },
-  });
 };
